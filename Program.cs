4c4e91acc9e525dbd393443588df8219cd7cc0f7
Coveragt <Coveragt@gmail.com>
2021-05-19 01:35:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IDEproject_No._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
