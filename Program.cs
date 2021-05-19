8d529a5b20791a365820c256767ebeff0d6b5d75
Coveragt <Coveragt@gmail.com>
2021-05-19 04:38:37 +0300

    v.0.1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace IDEproject_No._3
 {
@@ -6,7 +9,178 @@ namespace IDEproject_No._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var exit = "";
+            var students = new List<Student>();
+
+            do
+            {
+                string input = null;
+                Student student = null;
+
+                do
+                {
+                    Console.WriteLine("Please enter a student name and surname.");
+                    input = Console.ReadLine();
+                    if (input == null) continue;
+
+                    if (input.Split().Length == 2)
+                    {
+                        var studentNameSurname = input.Split();
+                        student = new Student(studentNameSurname[0], studentNameSurname[1]);
+                    }
+                    else
+                        input = null;
+
+                } while (input == null);
+
+                do
+                {
+                    Console.WriteLine(
+                        "Please enter student marks for the homeworks with a space in between each mark. (10 9 3 5 5)");
+                    input = Console.ReadLine();
+                    if (input == null) continue;
+
+                    if (IsDigitsOnly(input.Split(" ")) && MarkCheck(Array.ConvertAll(input.Split(" "), int.Parse)))
+                    {
+                        student.HwMarks = Array.ConvertAll(input.Split(" "), int.Parse);
+                        student.HwMarksList = input.Split(" ").Select(int.Parse).ToList();
+                    }
+                    else
+                        input = null;
+
+                } while (input == null);
+
+                do
+                {
+                    Console.WriteLine("Please enter a student mark for the exam.");
+                    input = Console.ReadLine();
+                    if (input != null)
+                    {
+                        if (IsDigitsOnly(input.Split(" ")) && MarkCheck(int.Parse(input)))
+                        {
+                            student.ExamMark = int.Parse(input);
+                        }
+                        else
+                            input = null;
+                    }
+                } while (input == null);
+                students.Add(student);
+
+                bool calculation = true;
+
+                do
+                {
+                    Console.WriteLine("Please enter 1 for average calculation or 2 for median calculation.");
+                    input = Console.ReadLine();
+                    if (input != null)
+                    {
+                        if (input == "1")
+                        {
+                            calculation = true;
+                        }
+                        else if (input == "2")
+                        {
+                            calculation = false;
+                        }
+                        else
+                            input = null;
+                    }
+                } while (input == null);
+
+                var header = "Surname         ";
+                var headerEnd = "";
+                var header2 = "";
+
+                if (calculation)
+                {
+                    headerEnd = "Name               Final Points(Avg.)";
+                    foreach (var VARIABLE in students)
+                    {
+                        VARIABLE.CalculateAvarage();
+                    }
+                }
+                else
+                {
+                    headerEnd = "Name               Final Points(Med.)";
+                    foreach (var VARIABLE in students)
+                    {
+                        VARIABLE.CalculateMedian();
+                    }
+                }
+
+                for (int i = 0; i <= header.Length + headerEnd.Length; i++)
+                {
+                    header2 += "-";
+                }
+
+
+                Console.Clear();
+                Console.WriteLine(header + headerEnd);
+                Console.WriteLine(header2);
+                foreach (var varStudent in students)
+                {
+                    string output = varStudent.Surname;
+                    for (int i = 0; i < header.Length - varStudent.Surname.Length; i++)
+                    {
+                        output += " ";
+                    }
+
+                    output += varStudent.Name;
+                    var finalPoints = $"{varStudent.Final_points:0.00}";
+                    for (int i = 0; i < headerEnd.Length - finalPoints.Length - varStudent.Name.Length; i++)
+                    {
+                        output += " ";
+                    }
+
+                    output += varStudent.Final_points;
+                    Console.WriteLine(output);
+                }
+
+
+                Console.WriteLine();
+                Console.WriteLine(
+                    @"If you would like to exit, type ""exit"". If you would like to repeat, just press enter.");
+                input = Console.ReadLine();
+                if (input != null)
+                {
+                    exit = input.ToUpper();
+                }
+            } while (exit != "EXIT");
+        }
+
+        static bool IsDigitsOnly(string[] str)
+        {
+            foreach (var variable in str)
+            {
+                foreach (char c in variable)
+                {
+                    if (c < '0' || c > '9')
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool MarkCheck(int[] homeworkMarks)
+        {
+            foreach (var homeworkMark in homeworkMarks)
+            {
+                if (homeworkMark < 1 || homeworkMark > 10)
+                    return false;
+            }
+
+            return true;
+        }
+
+        static bool MarkCheck(int examMark)
+        {
+            if (examMark < 1 || examMark > 10)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDEproject_No._3
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int[] HwMarks { get; set; }
+        public List<int> HwMarksList { get; set; }
+        public int ExamMark { get; set; }
+        public double Final_points { get; set; }
+
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+            var randNum = new Random();
+            HwMarks = new int[randNum.Next(0, 10)];
+
+            for (int i = 0; i < HwMarks.Length; i++)
+            {
+                HwMarks[i] = randNum.Next(1, 10);
+            }
+
+            ExamMark = randNum.Next(1, 10);
+        }
+
+        public void CalculateMedian()
+        {
+            var sortedPNumbers = Array.ConvertAll<int, double>(HwMarks, x => x);
+            Array.Sort(sortedPNumbers);
+
+            var size = sortedPNumbers.Length;
+            var mid = size / 2;
+            Final_points = size % 2 != 0 ? sortedPNumbers[mid] : (sortedPNumbers[mid] + sortedPNumbers[mid - 1]) / 2;
+        }
+
+        public void CalculateAvarage()
+        {
+            var avg = HwMarks.AsQueryable().Average();
+            Final_points = 0.3 * avg + 0.7 * ExamMark;
+        }
+    }
+}
