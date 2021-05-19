e85a4ca5a6d7343fce1e18cb2eec222d644a4ea1
Coveragt <Coveragt@gmail.com>
2021-05-19 06:21:08 +0300

    v0.5

--- StudentGenerator.cs
+++ StudentGenerator.cs
@@ -8,13 +8,37 @@ namespace IDEproject_No._3
 {
     class StudentGenerator
     {
-        public static List<Student> GenerateStudents(int amount)
+        public static List<Student> GenerateStudentsList(int amount)
         {
             List<Student> students = new List<Student>();
 
             for (int i = 0; i < amount; i++)
             {
                 students.Add(new Student(i));
+            }
+
+            return students;
+        }
+
+        public static LinkedList<Student> GenerateStudentsLinkedList(int amount)
+        {
+            LinkedList<Student> students = new LinkedList<Student>();
+
+            for (int i = 0; i < amount; i++)
+            {
+                students.AddLast(new Student(i));
+            }
+
+            return students;
+        }
+
+        public static Queue<Student> GenerateStudentsQueue(int amount)
+        {
+            Queue<Student> students = new Queue<Student>();
+
+            for (int i = 0; i < amount; i++)
+            {
+                students.Enqueue(new Student(i));
             }
 
             return students;
